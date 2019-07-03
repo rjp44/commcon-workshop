b2bb4710eddd7ebeeab5900b36e2cfaddeecda1d
Rob Pickering <rob@pickering.org>
2019-07-03 11:56:06 +0100

    First pass

---
+++ README.md
@@ -0,0 +1,5 @@
+# Commcon Workshop
+
+This is a collection of resources for CommCon Workshop
+
+## freeswitch - freeswitch instance connecting to Simwood and connecting to Google cloud services
---
+++ freeswitch/entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# run as user freeswitch
+FREESWITCH_USER=${FREESWITCH_USER:-freeswitch}
+FREESWITCH_DOMAIN=${FREESWITCH_DOMAIN:-localhost.localdomin}
+FREESWITCH_ORG=${FREESWITCH_ORG:-AcmeWidgets}
+
+SUBST=" "
+for VAR in $(env);
+do
+  IFS="=" read -r NAME VALUE <<<$VAR
+  EVALUE=$(sed -e 's/\//\\\//g' <<<$VALUE)
+  SUBST+=`printf "%ss/___%s___/%s/" " -e " "\\\$$NAME" "$EVALUE"`
+done
+
+
+if [ "$1" = "" ]; then
+  COMMAND="/usr/local/bin/freeswitch"
+else
+  COMMAND="$@"
+fi
+
+# Recursively copy any templated config files (with ENV substitution)
+for DIR in /templates/all /templates/${TRUNK_TYPE}
+do
+  if [ -d ${DIR} ]; then
+    cd ${DIR}
+    for FILE in `find . -type f -print`
+    do
+      echo $DIR $FILE
+      echo sed $SUBST
+      sed $SUBST <${FILE} >/${FILE}
+    done
+  else
+    echo NO Template for ${DIR} - this is probably bad.
+    exit 1;
+  fi
+done
+
+if [ "${FREESWITCH_UID}" != "" ] && [ "${FREESWITCH_GID}" != "" ]; then
+  # recreate user and group for freeswitch
+  deluser freeswitch && \
+  adduser --gecos "" --no-create-home --uid ${FREESWITCH_UID} --disabled-password ${FREESWITCH_USER} || exit
+  chown -R ${FREESWITCH_UID}:${FREESWITCH_UID} /usr/local/freeswitch \
+                                           /var/*/freeswitch
+fi
+
+exec ${COMMAND}
